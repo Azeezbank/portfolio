c804dfb2afb6f266cb3b5b7f30005e8b2059b925
Azeezbank <159575592+Azeezbank@users.noreply.github.com>
2024-03-26 14:57:54 +0100

    portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,2 +1,2 @@
-document.body.addEventListener("copy", (e) => {e.preventDefault()
+document.body.addEventListener("copy", (e) => {e.preventDefault();
 alert("Sorry, you cant copy from this site, contact the developr!")})

fce5eef961260f241ce09f4f3ed4d678fd62d333
Azeezbank <159575592+Azeezbank@users.noreply.github.com>
2024-07-08 19:44:03 +0100

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,2 +1,16 @@
 document.body.addEventListener("copy", (e) => {e.preventDefault();
 alert("Sorry, you cant copy from this site, contact the developr!")})
+
+document.addEventListener("DOMContentLoaded", function() {
+  let rotate = document.getElementById('rotate');
+  let images = ["url('https://tunstelecom.com.ng/wp-content/uploads/2024/06/20240611_091935_0_io_thread_1718092033461.png')",
+               "url('https://tunstelecom.com.ng/wp-content/uploads/2024/06/images-7.jpeg')",
+               "url('https://tunstelecom.com.ng/wp-content/uploads/2024/06/images-2.jpeg')"];
+  let currentIndex = 0;
+  function changeImage() {
+  rotate.src = images[currentIndex]
+  currentIndex = (currentIndex + 1) % images.length;
+  }
+  setInterval(changeImage, 5000);
+  changeImage();
+});

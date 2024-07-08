5421e3d477da03b9c6316776475df20c401b2d3e
Azeezbank <159575592+Azeezbank@users.noreply.github.com>
2024-07-08 20:34:22 +0100

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -8,8 +8,11 @@ document.addEventListener("DOMContentLoaded", function() {
                "url('https://tunstelecom.com.ng/wp-content/uploads/2024/06/images-2.jpeg')"];
   let currentIndex = 0;
   function changeImage() {
+    let rotate = document.createElement('img')
   rotate.src = images[currentIndex]
   currentIndex = (currentIndex + 1) % images.length;
+    let rotate = document.getElementById('rotate')
+    rotate.appendchild(rotate)
   }
   setInterval(changeImage, 5000);
   changeImage();

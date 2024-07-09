004aa10dba20978779e5655add7d5cd5cf97f270
Azeezbank <159575592+Azeezbank@users.noreply.github.com>
2024-07-09 17:41:19 +0100

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -5,7 +5,8 @@ alert("Sorry, you cant copy from this site, contact the developr!")})
   let images = ['https://tunstelecom.com.ng/wp-content/uploads/2024/07/Maize-Farming-Farmsquare.jpg',
                'https://tunstelecom.com.ng/wp-content/uploads/2024/07/Harvesting-honey-from-a-bee-hive-at-the-IITA-Forest-Center.jpg',
                'https://tunstelecom.com.ng/wp-content/uploads/2024/07/images-5.jpeg',
-               'https://tunstelecom.com.ng/wp-content/uploads/2024/06/images-7.jpeg'
+               'https://tunstelecom.com.ng/wp-content/uploads/2024/06/images-7.jpeg',
+                'https://tunstelecom.com.ng/wp-content/uploads/2024/06/images-5.jpeg,
     ];
   let currentIndex = 0;
   function changeImage() {

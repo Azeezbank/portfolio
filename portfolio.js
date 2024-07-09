95c2a29cdb558d0450e7620dc98c7511b24e7a78
Azeezbank <159575592+Azeezbank@users.noreply.github.com>
2024-07-09 16:56:08 +0100

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -2,13 +2,17 @@ document.body.addEventListener("copy", (e) => {e.preventDefault();
 alert("Sorry, you cant copy from this site, contact the developr!")})
 
   // Rotating image
+document.addEventListener('DOMContentLoaded', () => {
   let images = ['https://tunstelecom.com.ng/wp-content/uploads/2024/07/Maize-Farming-Farmsquare.jpg',
                'https://tunstelecom.com.ng/wp-content/uploads/2024/07/Harvesting-honey-from-a-bee-hive-at-the-IITA-Forest-Center.jpg',
-               'https://tunstelecom.com.ng/wp-content/uploads/2024/07/images-5.jpeg'];
+               'https://tunstelecom.com.ng/wp-content/uploads/2024/07/images-5.jpeg'
+               'https://tunstelecom.com.ng/wp-content/uploads/2024/06/images-7.jpeg'
+    ];
   let currentIndex = 0;
   function changeImage() {
     let rotate = document.getElementById('rotate')
     rotate.src = images[currentIndex]
   currentIndex = (currentIndex + 1) % images.length;
   }
-setInterval(changeImage, 5000)
+setInterval(changeImage, 5000);
+});

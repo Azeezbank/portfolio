4821eec5f85f52f8a68aa5229f5fe8d72aaf083f
Azeezbank <159575592+Azeezbank@users.noreply.github.com>
2024-07-09 15:28:21 +0100

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,18 +1,14 @@
 document.body.addEventListener("copy", (e) => {e.preventDefault();
 alert("Sorry, you cant copy from this site, contact the developr!")})
 
-  //let rotate = document.getElementById('rotate');
-  let images = ["url('https://tunstelecom.com.ng/wp-content/uploads/2024/06/20240611_091935_0_io_thread_1718092033461.png')",
-               "url('https://tunstelecom.com.ng/wp-content/uploads/2024/06/images-7.jpeg')",
-               "url('https://tunstelecom.com.ng/wp-content/uploads/2024/06/images-2.jpeg')"];
+  // Rotating image
+  let images = ['https://farmsquare.ng/wp-content/uploads/2021/07/Maize-Farming-Farmsquare.jpg',
+               'http://www.iita.org/wp-content/uploads/2022/05/Harvesting-honey-from-a-bee-hive-at-the-IITA-Forest-Center.jpg',
+               'https://www.researchgate.net/profile/Gabriel-Akunna/publication/315063882/figure/fig1/AS:472175124717570@1489586730755/Fig-no-1-Animal-treatment-with-alloxan-through-the-penial-vein-while-lying-on-its-back.png'];
   let currentIndex = 0;
-let rotate = document.createElement('img')
   function changeImage() {
+    let rotate = document.getElementById('rotate')
     rotate.src = images[currentIndex]
-    rotate.alt = "image";
-    rotate.width = 100%;
   currentIndex = (currentIndex + 1) % images.length;
-    document.getElementById('Agriculture').appendChild('rotate')
   }
-document.getElementById('Agriculture').appendChild('rotate')
-  setInterval(changeImage, 5000);
+setInterval(changeImage, 5000)

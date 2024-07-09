4d453b14fdd0fb37eef975065780b04b5ad9f985
Azeezbank <159575592+Azeezbank@users.noreply.github.com>
2024-07-09 15:39:47 +0100

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -2,9 +2,9 @@ document.body.addEventListener("copy", (e) => {e.preventDefault();
 alert("Sorry, you cant copy from this site, contact the developr!")})
 
   // Rotating image
-  let images = ['https://farmsquare.ng/wp-content/uploads/2021/07/Maize-Farming-Farmsquare.jpg',
-               'http://www.iita.org/wp-content/uploads/2022/05/Harvesting-honey-from-a-bee-hive-at-the-IITA-Forest-Center.jpg',
-               'https://www.researchgate.net/profile/Gabriel-Akunna/publication/315063882/figure/fig1/AS:472175124717570@1489586730755/Fig-no-1-Animal-treatment-with-alloxan-through-the-penial-vein-while-lying-on-its-back.png'];
+  let images = ['https://tunstelecom.com.ng/wp-content/uploads/2024/07/Maize-Farming-Farmsquare.jpg',
+               'https://tunstelecom.com.ng/wp-content/uploads/2024/07/Harvesting-honey-from-a-bee-hive-at-the-IITA-Forest-Center.jpg',
+               'https://tunstelecom.com.ng/wp-content/uploads/2024/07/images-5.jpeg'];
   let currentIndex = 0;
   function changeImage() {
     let rotate = document.getElementById('rotate')

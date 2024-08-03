36051bd93b276522405c61d479ddc2db412593b7
Azeezbank <159575592+Azeezbank@users.noreply.github.com>
2024-08-03 11:46:50 +0100

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -2,11 +2,14 @@ document.body.addEventListener("copy", (e) => {e.preventDefault();
 alert("Sorry, you cant copy from this site, contact the developr!")})
 
   // Rotating image
-  let images = ['https://tunstelecom.com.ng/wp-content/uploads/2024/07/Maize-Farming-Farmsquare.jpg',
-               'https://tunstelecom.com.ng/wp-content/uploads/2024/07/Harvesting-honey-from-a-bee-hive-at-the-IITA-Forest-Center.jpg',
+  let images = ['https://tunstelecom.com.ng/wp-content/uploads/2024/07/images-7.jpeg',
+               'https://tunstelecom.com.ng/wp-content/uploads/2024/07/images-8.jpeg',
                'https://tunstelecom.com.ng/wp-content/uploads/2024/07/images-5.jpeg',
-               'https://tunstelecom.com.ng/wp-content/uploads/2024/06/images-7.jpeg',
-                'https://tunstelecom.com.ng/wp-content/uploads/2024/06/images-5.jpeg',
+               'https://tunstelecom.com.ng/wp-content/uploads/2024/07/images-10.jpeg',
+                'https://tunstelecom.com.ng/wp-content/uploads/2024/07/images-6.jpeg',
+                'https://tunstelecom.com.ng/wp-content/uploads/2024/07/images-13.jpeg',
+                'https://tunstelecom.com.ng/wp-content/uploads/2024/07/images-12.jpeg',
+                'https://tunstelecom.com.ng/wp-content/uploads/2024/07/images-2.jpeg',
     ];
   let currentIndex = 0;
   function changeImage() {
